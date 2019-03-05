74a1f08e15ca63a90d9df9178a389bf94d3dfb0e
cecgresonant <a.c.larsen@fys.uio.no>
2019-03-05 09:26:12 +0100

    Added root to mama script

---
+++ th22mama.C
@@ -0,0 +1,52 @@
+/*
+ Usage: .x th22mama.C+ ( matrix, "output-filename.m") or .x th22mama.C+ ( matrix, "output-filename.m", "comment")
+*/
+
+#include <TH2.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <time.h>
+
+void th22mama(TH2* m, const char* filename, const char* comment="none")
+{
+    char tmp[128];
+    time_t now = time(0);
+    strftime(tmp, sizeof(tmp), "%Y-%m-%d %T", localtime(&now)); // not the 23-Mar-07 16:02:34 format
+    
+    TAxis *xax = m->GetXaxis(), *yax = m->GetYaxis();
+    
+    const int nx = std::min(xax->GetNbins(), 2048);
+    const int ny = yax->GetNbins();
+    std::cout << "matrix is " << nx << 'x' << ny << "; comment='" << comment << "'" << std::endl;
+    
+    std::ofstream mama(filename);
+    mama << "!FILE=Disk \n"
+    "!KIND=Matrix \n"
+    "!LABORATORY=Oslo Cyclotron Laboratory (OCL) \n"
+    "!EXPERIMENT=siri2root \n"
+    "!COMMENT=" << comment << "\n"
+    "!TIME=" << tmp << "\n"
+    "!CALIBRATION EkeV=6";
+    const float cal[6] = { xax->GetBinLowEdge(1), xax->GetBinWidth(1), 0,
+        yax->GetBinLowEdge(1), yax->GetBinWidth(1), 0 };
+    for(int i=0; i<6; ++i) {
+        snprintf(tmp, sizeof(tmp), ",%13.6E", cal[i]);
+        mama << tmp;
+    }
+    mama << "\n!PRECISION=16\n";
+    snprintf(tmp, sizeof(tmp), "!DIMENSION=2,0:%4d,0:%4d\n", nx-1, ny-1);
+    mama << tmp;
+    snprintf(tmp, sizeof(tmp), "!CHANNEL=(0:%4d,0:%4d)\n", nx-1, ny-1);
+    mama << tmp;
+    
+    for(int iy=1; iy<=ny; ++iy) {
+        for(int ix=1; ix<=nx; ++ix)
+            mama << m->GetBinContent(ix, iy) << ' ';
+        mama << "\n";
+    }
+    mama << "!IDEND=" << endl << endl;
+    
+    mama.close();
+}
